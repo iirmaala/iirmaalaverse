60e84311fb4de62e7e78f52aec40c7f1cb0c485a
iirmaala <intannirmalaa668@gmail.com>
2026-02-04 20:27:46 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,54 @@
-self.addEventListener('install', e => {console.log("Service Worker: Installed");});
-self.addEventListener('fetch', e => {});
+// Nama cache versi terbaru
+const CACHE_NAME = 'eleventwo-cache-v2';
+
+// File-file yang mau di-cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/service-worker.js',
+  '/images/icon.jpeg',
+  '/images/sampul.jpg',
+  '/images/background.jpg'
+];
+
+// Install service worker & cache file
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened and files added');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch request: ambil dari cache dulu, kalau ga ada baru fetch online
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate: hapus cache lama kalau ada update
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              console.log('Deleting old cache:', cacheName);
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+  );
+});
